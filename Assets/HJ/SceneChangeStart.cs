58a0ab866fd5d214f07161006441e96ec5859a8d
Chohajin <hajin1742@gmail.com>
2026-07-14 17:37:16 +0900

    changeClassName

--- Assets/HJ/SceneChangeStart.cs
+++ Assets/HJ/SceneChangeStart.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; 
 
-public class SceneChanger : MonoBehaviour
+public class SceneChangeStart : MonoBehaviour
 {
     public void ChangeToGameScene()
     {
