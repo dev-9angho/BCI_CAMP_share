00aff3e2a6118578968301979ebc4cc82a6cedeb
dev-9angho <qkrrkdgh0602@naver.com>
2026-07-14 16:34:54 +0900

    first setting
    first setting

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Rigidbody rigid;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        speed = 0.1f;
+        rigid = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v) * speed);
+    }
+}
